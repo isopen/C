d8c132e3d94ef59cd7e62d06a6303c067b265dda
isopen <mr_makss@mail.ru>
2017-03-25 16:00:42 +0700

    Update main.cpp

--- task1/server/main.cpp
+++ task1/server/main.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 int main() {
 
-	pid_t pid;
-	switch(pid = fork()) {
-  case -1: perror("fork");
-	   exit(1);
-	case 0: setsid();
-		Server server;
-		server.start();
-	}
+  pid_t pid;
+  switch(pid = fork()) {
+    case -1: perror("fork");
+	     exit(1);
+    case 0: setsid();
+	    Server server;
+	    server.start();
+  }
 
-	return 0;
+  return 0;
 
 }
