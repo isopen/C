652607ff4d4f3d2cccbd139a87de035a52a2ac33
mr_makss <mr_makss@mail.ru>
2017-03-27 18:25:30 +0600

    test

--- task1/server/test.cpp
+++ task1/server/test.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <thread>
+#include <mutex>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,7 +9,7 @@
 
 using namespace std;
 
-void client_handler() {
+void client_handler(mutex &mtx) {
 
   int s = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in sa;
@@ -17,20 +18,26 @@ void client_handler() {
 	sa.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	connect(s, (struct sockaddr *) &sa, sizeof(sa));
 
-	char buff[] = "PING";
-	send(s, buff, 4, MSG_NOSIGNAL);
-  cout << to_string(rand()) + "\n";
-  /*while(true) {
-    cout << rand();
-  }*/
+	//char buff[] = "PING";
+	//send(s, buff, 4, MSG_NOSIGNAL);
+
+  while(true) {
+    mtx.lock();
+    cout << endl << this_thread::get_id() << endl;
+    mtx.unlock();
+    usleep(5000);
+  }
+
 }
 
 int main(int argc, char **argv) {
 
-  for(int i = 0; i < 10000; i++) {
-    thread t1(client_handler);
-    t1.join();
-  }
+  mutex mtx;
+  thread t1(client_handler, ref(mtx));
+  thread t2(client_handler, ref(mtx));
+
+  t1.join();
+  t2.join();
 
   return 0;
 
