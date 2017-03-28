c9a0e13538fdd132e27631dcfbb8d8997b77bbc4
mr_makss <mr_makss@mail.ru>
2017-03-28 17:01:22 +0600

    test

--- task1/server/main.cpp
+++ task1/server/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <locale>
 #include "src/server.h"
 
 using namespace std;
@@ -15,15 +16,15 @@ int main() {
       case -1: perror("fork");
                exit(1);
       case 0: setsid();
-              Server *server;
-              server->start();
+              Server server;
+              server.start();
     }
 
   }else {
 
     Server server;
     server.start();
-    
+
   }
 
   return 0;
