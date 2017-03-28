e5078fa8332b0ad65d904f0e8eb86b57d54c214b
mr_makss <mr_makss@mail.ru>
2017-03-28 11:02:53 +0600

    test

--- task1/client/main.cpp
+++ task1/client/main.cpp
@@ -3,22 +3,39 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <iostream>
+#include <cstring>
+#include <thread>
 
 using namespace std;
 
+void request_get(int sock) {
+	while(true) {
+		static char Buffer[1024];
+		recv(sock, Buffer, 1024, 0);
+		cout << Buffer;
+	}
+}
+
 int main(int argc, char **argv) {
 
-	int s = socket(AF_INET, SOCK_STREAM, 0);
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in sa;
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(12345);
 	sa.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-	connect(s, (struct sockaddr *) &sa, sizeof(sa));
+	connect(sock, (struct sockaddr *) &sa, sizeof(sa));
 
-	char buff[] = "PING";
-	send(s, buff, 4, MSG_NOSIGNAL);
-	shutdown(s, SHUT_RDWR);
-	close(s);
+	thread t1(request_get, ref(sock));
+	t1.detach();
+
+	string message;
+	while(getline(cin, message)) {
+		const char *ptr = (message + "\n").c_str();
+		send(sock, ptr, (message.size() + 1), MSG_NOSIGNAL);
+	}
+
+	shutdown(sock, SHUT_RDWR);
+	close(sock);
 
 	return 0;
 
